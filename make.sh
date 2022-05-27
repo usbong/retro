cf707d1c4d0c706b8b26f240b03acc194b2afd01
Masarap Mabuhay <masarapmabuhay@gmail.com>
2022-05-27 10:23:45 +0800

    Add files via upload

--- make.sh
+++ make.sh
@@ -11,12 +11,18 @@
 # @company: USBONG
 # @author: SYSON, MICHAEL B.
 # @date created: 20200930
-# @last updated: 20220516; from 20210818
+# @last updated: 20220527; from 20220516
 # @website address: http://www.usbong.ph
 #
 # Reference:
 # 1) https://www3.ntu.edu.sg/home/ehchua/programming/cpp/gcc_make.html;
 # last accessed: 20200930
+#
+# Notes:
+# 1) No Open Graphics Library (OpenGL)
+# 
+# 2) executes: on 32-bit Operating Systems without 3D Graphics Acceleration
+
 
 #removed by Mike, 20210818
 #cp -r ./textures ./output/
@@ -52,7 +58,7 @@ g++ -c mainLinux.cpp
 #g++ -o ./output/UsbongStoryLinux mainLinux.o OpenGLCanvas.o MyDynamicObject.o RobotShip.o Font.o Beam.o Asteroid.o Level.o PolygonUtils.o CTargaImage.o Pilot.o UsbongUtils.o Button.o Ball.o Text.o Level2D.o -lGL -lGLU -lglut
 #edited by Mike, 20210818
 #g++ -o ./output/UsbongStoryLinux mainLinux.o OpenGLCanvas.o MyDynamicObject.o RobotShip.o Font.o Beam.o Asteroid.o Level.o PolygonUtils.o CTargaImage.o Pilot.o UsbongUtils.o Button.o Ball.o Text.o Level2D.o -lGL -lGLU -lglut -lSDL2 -lSDL2_image
-g++ -o ./output/UsbongRetroLinux mainLinux.o -lGL -lGLU -lglut -lSDL2 -lSDL2_image
+g++ -o ./output/UsbongRetroSystemLinux mainLinux.o -lGL -lGLU -lglut -lSDL2 -lSDL2_image
 
 #added by Mike, 20210220; removed by Mike, 20210818
 #cp -r ./*.o ./objectFilesLinux/
